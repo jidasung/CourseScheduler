3c0f42e9bf3a6bdaaddfce2cafe496f217310c36
Hyeong-Gyu Jeong <hunguenglish@gmail.com>
2014-11-20 00:13:33 +0900

    Working on Menu

--- ProjectA/Course.h
+++ ProjectA/Course.h
@@ -1,2 +1,65 @@
 #include <iostream>
+#include <string>
 
+using namespace std;
+
+class Course {
+private:
+	int sectionNo;
+	string subject;
+	Room room;
+	string instructor_name;
+	char day;
+	Time timeStart;
+	Time timeEnd;
+public:
+};
+
+class Student {
+private:
+	int studentNo;
+	int password;
+	string name;
+	Course course;
+public:
+};
+
+class Instructor {
+private:
+	int instructorNo;
+	int password;
+	string name;
+	int rank;
+public:
+};
+
+class Room {
+private:
+	int roomNo;
+	string description;
+	int type;
+	int addRoom();
+public:
+};
+
+class Subject {
+private:
+	int subjectNo;
+	string description;
+	int unit;
+	int addSubject();
+public:
+};
+
+class StudentSchedule {
+private:
+	int studentNo;
+	int sectionNo;
+	int noConflict();
+public:
+};
+
+class Time {
+	int hour;
+	int minute;
+};
